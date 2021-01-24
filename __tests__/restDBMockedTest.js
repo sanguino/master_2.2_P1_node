ca5342de5f8abed1a20a058cfd586b0dac5af94a
Miguel G. Sanguino <sanguino@me.com>
2021-01-24 22:22:15 +0100

    chore mocked test code style

--- __tests__/restDBMockedTest.js
+++ __tests__/restDBMockedTest.js
@@ -6,7 +6,7 @@ const AWS = require('aws-sdk');
 jest.mock('aws-sdk');
 
 const putMock = (params, cb) => {
-  cb(undefined, params)
+  cb(undefined, params);
 };
 
 const scanMock = (params, cb) => {
@@ -15,11 +15,10 @@ const scanMock = (params, cb) => {
       {"title": "film1", "synopsis": "synopsis1", "rate": 3},
       {"title": "film2", "synopsis": "synopsis2", "rate": 4}
     ]
-  })
+  });
 };
 
 beforeAll(async () => {
-
   AWS.DynamoDB.DocumentClient.mockImplementation(() => {
     return {
       put: jest.fn().mockImplementation(putMock),
@@ -31,20 +30,19 @@ beforeAll(async () => {
 
 test('Retrieve all films', async () => {
   const response = await request.get('/api/films/')
-    .expect(200)
+    .expect(200);
 
-  expect(response.body[0]['title']).toBe("film1")
-  expect(response.body[1]['title']).toBe("film2")
-})
+  expect(response.body[0]['title']).toBe("film1");
+  expect(response.body[1]['title']).toBe("film2");
+});
 
 
 test('Create a new film', async () => {
-  const film = {"title": "film1", "synopsis": "synopsis1", "rate": 3}
+  const film = {"title": "film1", "synopsis": "synopsis1", "rate": 3};
   const response = await request.post('/api/films/')
     .send(film)
-    .expect(201)
-  expect(response.body['title']).toBe("film1")
-  expect(response.body['synopsis']).toBe("synopsis1")
+    .expect(201);
 
-})
-
+  expect(response.body['title']).toBe("film1");
+  expect(response.body['synopsis']).toBe("synopsis1");
+});
