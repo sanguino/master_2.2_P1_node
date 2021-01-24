3ff0aabb72a193ba98b41214dfb1f4374dfd6621
Miguel G. Sanguino <sanguino@me.com>
2021-01-24 22:22:30 +0100

    fix testContainer test

--- __tests__/restTestContainerTest.js
+++ __tests__/restTestContainerTest.js
@@ -1,19 +1,21 @@
 const supertest = require('supertest');
 const AWS = require('aws-sdk');
-const { GenericContainer } = require("testcontainers");
+const {GenericContainer} = require("testcontainers");
 
 const app = require('../src/app');
-const createTableIfNotExist = require("../src/db/createTable")
+const createTableIfNotExist = require("../src/db/createTable");
 
 let dynamoContainer;
 let request;
 
-jest.setTimeout(60000)
+jest.setTimeout(60000);
 
 beforeAll(async () => {
-  dynamoContainer = await new GenericContainer("amazon/dynamodb-local","1.13.6")
+  dynamoContainer = await new GenericContainer("amazon/dynamodb-local", "1.13.6")
     .withExposedPorts(8000)
-    .start().catch((err)=>{console.log(err)})
+    .start().catch((err) => {
+      console.log(err)
+    });
 
   AWS.config.update({
     region: 'local',
@@ -26,31 +28,38 @@ beforeAll(async () => {
   request = supertest(app);
 });
 
+afterEach(async () => {
+  await new AWS.DynamoDB().deleteTable({TableName: "films"}).promise();
+  await createTableIfNotExist("films");
+})
+
 afterAll(async () => {
   await dynamoContainer.stop();
 });
 
 test('Retrieve all films', async () => {
-  const film1 = {"title": "film1", "synopsis": "synopsis1", "rate": 3}
-  const film2 = {"title": "film2", "synopsis": "synopsis2", "rate": 3}
+  await request.post('/api/films/').send({"title": "film1", "synopsis": "synopsis1"});
+  await request.post('/api/films/').send({"title": "film2", "synopsis": "synopsis2"});
 
-  await request.post('/api/films/').send(film1);
-  await request.post('/api/films/').send(film2);
+  const response = await request.get('/api/films/').expect(200);
 
-  const response = await request.get('/api/films/')
-    .expect(200)
-
-  expect(response.body[0]['title']).toBe("film1")
-  expect(response.body[1]['title']).toBe("film2")
-})
+  expect(response.body.length).toBe(5);
+  expect(response.body).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({"title": "film1"}),
+      expect.objectContaining({"title": "film2"})
+    ])
+  );
+});
 
 
 test('Create a new film', async () => {
-  const film = {"title": "film1", "synopsis": "synopsis1", "rate": 3}
+  const film = {"title": "film3", "synopsis": "synopsis3"};
   const response = await request.post('/api/films/')
     .send(film)
-    .expect(201)
-  expect(response.body['title']).toBe("film1")
-  expect(response.body['synopsis']).toBe("synopsis1")
+    .expect(201);
+
+  expect(response.body['title']).toBe("film3");
+  expect(response.body['synopsis']).toBe("synopsis3");
 })
 
